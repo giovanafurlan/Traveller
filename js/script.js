d53c96bdbe7788dba2dd4724b0512706c3fc3ff6
giovanafurlan <RM88936@fiap.com.br>
2021-06-07 10:32:20 -0300

    Iniciando JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// document.querySelector('#botao').addEventListener('click', ()=> {
+//     var signo = prompt("Qual é o seu signo?");
+//     alert('Seu email é ' + signo);
+// });
+
+
+import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
