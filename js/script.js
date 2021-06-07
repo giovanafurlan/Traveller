ec446adda755e88bd3a585d4eb3c29f0a4b36075
giovanafurlan <RM88936@fiap.com.br>
2021-06-07 17:12:06 -0300

    Adicionando comando JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-document.querySelector('#botao').addEventListener('click', ()=> {
-    var problema = prompt("Reportar um problema");
-    alert("Seu problema " + problema + " foi reportado");
-});
+// document.querySelector('#botao').addEventListener('click', ()=> {
+//     var problema = prompt("Reportar um problema");
+//     alert("Seu problema " + problema + " foi reportado");
+// });
 
 
 // import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
+
+function reportar() {  
+    prompt ("Reportar um problema da plataforma");  
+    } 
